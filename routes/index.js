949019f26f47528a413293c5d98d5c3a22071465
Donald.Gifford <donald.gifford@disney.com>
2017-11-25 21:10:18 -0800

    update for env var

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,9 @@ var express = require('express');
 var router = express.Router();
 var config = require('config');
 /* GET home page. */
+const api_url = process.env.API_URL || "http://localhost:8080/health";
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express', apiUrl:config.api_url });
+  res.render('index', { title: 'Express', apiUrl:api_url });
 });
 
 module.exports = router;
